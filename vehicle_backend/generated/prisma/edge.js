7caff934b2540a943195bfff0dd57ecbec255835
kavya <bonagiri.kavya.31@gmail.com>
2025-05-09 18:03:21 +0530

    4th commit seeding done for vehicle and vehicletype

--- vehicle_backend/generated/prisma/edge.js
+++ vehicle_backend/generated/prisma/edge.js
@@ -165,11 +165,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:310300@localhost:5432/vehicle-db?schema=public"
+        "value": null
       }
     }
   },
